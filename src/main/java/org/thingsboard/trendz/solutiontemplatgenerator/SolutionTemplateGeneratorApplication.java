963f0bc446f6415f4d0d4960a7b0a0053cc9eeab
Anatolii Davydko <anatoly.davydko@gmail.com>
2022-09-22 15:24:44 +0300

    Initialize project

---
+++ src/main/java/org/thingsboard/trendz/solutiontemplatgenerator/SolutionTemplateGeneratorApplication.java
@@ -0,0 +1,12 @@
+package org.thingsboard.trendz.solutiontemplatgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionTemplateGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionTemplateGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/org/thingsboard/trendz/solutiontemplatgenerator/SolutionTemplateGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thingsboard.trendz.solutiontemplatgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionTemplateGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
