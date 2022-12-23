ee19e953ea1a1268a71a1e1ba52b191a42dc6db4
Anatolii Davydko <adavydko@thingsboard.io>
2022-12-23 15:53:06 +0200

    add some apis (not tested)

--- src/main/java/org/thingsboard/trendz/generator/service/TbRestClient.java
+++ src/main/java/org/thingsboard/trendz/generator/service/TbRestClient.java
@@ -14,12 +14,15 @@ import org.thingsboard.server.common.data.asset.Asset;
 import org.thingsboard.server.common.data.group.EntityGroup;
 import org.thingsboard.server.common.data.id.CustomerId;
 import org.thingsboard.server.common.data.id.TenantId;
+import org.thingsboard.server.common.data.rule.RuleChain;
 import org.thingsboard.trendz.generator.model.AuthToken;
 import org.thingsboard.trendz.generator.model.LoginRequest;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Slf4j
@@ -61,6 +64,43 @@ public class TbRestClient {
     }
 
 
+    public Optional<Customer> getCustomerById(UUID customerId) {
+        try {
+            Customer customer = restTemplate.getForEntity(baseURL + "/api/customer/" + customerId.toString(), Customer.class).getBody();
+            return Optional.ofNullable(customer);
+        } catch (HttpClientErrorException.NotFound e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Asset> getAssetById(UUID assetId) {
+        try {
+            Asset asset = restTemplate.getForEntity(baseURL + "/api/asset/" + assetId.toString(), Asset.class).getBody();
+            return Optional.ofNullable(asset);
+        } catch (HttpClientErrorException.NotFound e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Device> getDeviceById(UUID deviceId) {
+        try {
+            Device device = restTemplate.getForEntity(baseURL + "/api/device/" + deviceId.toString(), Device.class).getBody();
+            return Optional.ofNullable(device);
+        } catch (HttpClientErrorException.NotFound e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<RuleChain> getRuleChainById(UUID ruleChainId) {
+        try {
+            RuleChain ruleChain = restTemplate.getForEntity(baseURL + "/api/device/" + ruleChainId.toString(), RuleChain.class).getBody();
+            return Optional.ofNullable(ruleChain);
+        } catch (HttpClientErrorException.NotFound e) {
+            return Optional.empty();
+        }
+    }
+
+
     public Optional<Customer> getCustomerByTitle(String title) {
         try {
             Customer customer = restTemplate.getForEntity(baseURL + "/api/tenant/customers?customerTitle=" + title, Customer.class).getBody();
@@ -70,7 +110,7 @@ public class TbRestClient {
         }
     }
 
-    public Optional<Asset> getAssetByTitle(String name) {
+    public Optional<Asset> getAssetByName(String name) {
         try {
             Asset asset = restTemplate.getForEntity(baseURL + "/api/tenant/assets?assetName=" + name, Asset.class).getBody();
             return Optional.ofNullable(asset);
@@ -79,13 +119,34 @@ public class TbRestClient {
         }
     }
 
-    public Optional<Device> getDeviceByTitle(String name) {
+    public Optional<Device> getDeviceByName(String name) {
         try {
             Device device = restTemplate.getForEntity(baseURL + "/api/tenant/devices?deviceName=" + name, Device.class).getBody();
             return Optional.ofNullable(device);
         } catch (HttpClientErrorException.NotFound e) {
             return Optional.empty();
         }
+    }
+
+
+    public Set<Customer> getAllCustomers() {
+        Set<Customer> typeKeeper = new HashSet<>();
+        return restTemplate.getForEntity(baseURL + "/api/customers", typeKeeper.getClass()).getBody();
+    }
+
+    public Set<Asset> getAllAssets() {
+        Set<Asset> typeKeeper = new HashSet<>();
+        return restTemplate.getForEntity(baseURL + "/api/tenant/assetInfos", typeKeeper.getClass()).getBody();
+    }
+
+    public Set<Device> getAllDevices() {
+        Set<Device> typeKeeper = new HashSet<>();
+        return restTemplate.getForEntity(baseURL + "/api/tenant/deviceInfos", typeKeeper.getClass()).getBody();
+    }
+
+    public Set<RuleChain> getAllRuleChains() {
+        Set<RuleChain> typeKeeper = new HashSet<>();
+        return restTemplate.getForEntity(baseURL + "/api/ruleChains", typeKeeper.getClass()).getBody();
     }
 
 
@@ -109,6 +170,12 @@ public class TbRestClient {
         return restTemplate.postForEntity(baseURL + "/api/device", device, Device.class).getBody();
     }
 
+    public RuleChain createRuleChain(String name) {
+        RuleChain ruleChain = new RuleChain();
+        ruleChain.setName(name);
+        return restTemplate.postForEntity(baseURL + "/api/customer", ruleChain, RuleChain.class).getBody();
+    }
+
 
     public void deleteCustomer(UUID customerId) {
         restTemplate.delete(baseURL + "/api/customer/" + customerId);
@@ -120,6 +187,10 @@ public class TbRestClient {
 
     public void deleteDevice(UUID deviceId) {
         restTemplate.delete(baseURL + "/api/device/" + deviceId);
+    }
+
+    public void deleteRuleChain(UUID ruleChainId) {
+        restTemplate.delete(baseURL + "/api/ruleChain/" + ruleChainId);
     }
 
 
