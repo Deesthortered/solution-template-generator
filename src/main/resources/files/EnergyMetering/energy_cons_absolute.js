42ce35ae1b04acef4e42cd207822a684b46f4fea
Anatolii Davydko <anatoly.davydko@gmail.com>
2023-02-01 15:00:30 +0200

    fixed energy cons js

--- src/main/resources/files/EnergyMetering/energy_cons_absolute.js
+++ src/main/resources/files/EnergyMetering/energy_cons_absolute.js
@@ -1,6 +1,15 @@
-var heatConsAbsolute = parseInt(metadata.heatConsAbsolute);
+var energyConsAbsolutePoint = JSON.parse(metadata.energyConsAbsolute);
+var point_ts = energyConsAbsolutePoint.ts;
+var point_value = energyConsAbsolutePoint.value;
+
+var data_ts = msg.ts;
+var data_value = msg.values.energyConsumption;
+
 var newMsg = {
-    energyConsAbsolute: heatConsAbsolute + msg.values.energyConsumption
+    "ts" : data_ts,
+    "values" : {
+        "energyConsAbsolute" : point_value + data_value
+    }
 }
 return {
     msg: newMsg,
--- src/main/resources/files/EnergyMetering/heat_cons_absolute.js
+++ src/main/resources/files/EnergyMetering/heat_cons_absolute.js
@@ -1,6 +1,15 @@
-var heatConsAbsolute = parseInt(metadata.heatConsAbsolute);
+var heatConsAbsolutePoint = JSON.parse(metadata.heatConsAbsolute);
+var point_ts = heatConsAbsolutePoint.ts;
+var point_value = heatConsAbsolutePoint.value;
+
+var data_ts = msg.ts;
+var data_value = msg.values.heatConsumption;
+
 var newMsg = {
-    heatConsAbsolute: heatConsAbsolute + msg.values.heatConsumption
+    "ts" : data_ts,
+    "values" : {
+        "heatConsAbsolute" : point_value + data_value
+    }
 }
 return {
     msg: newMsg,
