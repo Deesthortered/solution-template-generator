bb2a42aec8fb746517f5aa5c44b089f8378f0a58
Anatolii Davydko <adavydko@thingsboard.io>
2023-04-21 16:34:04 +0300

    greenhouse, rule chain (16)

--- src/main/resources/files/Greenhouse/soil_acidity.js
+++ src/main/resources/files/Greenhouse/soil_acidity.js
@@ -1,1 +1,37 @@
+var getRandomInt = function (min, max) {
+    if (min === max) {
+        return min;
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var makeAcidityData = function () {
+    var period = parseInt(metadata.ss_maxRipeningPeriodDays) * 24;
+    var minLevel = parseInt(metadata.ss_minPh);
+    var maxLevel = parseInt(metadata.ss_maxPh);
+
+    var increaseLevel = (maxLevel - minLevel) / period;
+    var irrigationIncreaseLevel = (maxLevel - minLevel) / period * 24 * 5;
+    var acidificationDecreaseLevel = (maxLevel - minLevel);
+    var currentLevel = parseInt(metadata.acidity);
+
+    var irrigation = metadata.values_irrigation !== null ? metadata.values_irrigation : false;
+
+    currentLevel -= increaseLevel;
+    currentLevel -= (irrigation) ? irrigationIncreaseLevel : 0;
+
+    if (currentLevel <= minLevel) {
+        metadata.values_acidification = true;
+        currentLevel += acidificationDecreaseLevel;
+    }
+
+    currentLevel += getRandomInt(-0.03, 0.03);
+
+    return currentLevel;
+}
+
+metadata.values_acidity = makeAcidityData();
+
 return {msg: msg, metadata: metadata, msgType: msgType};
